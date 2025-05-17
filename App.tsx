41dfab9a4e3d576f8d38f18855b96bd8f03ab24c
Nikhileshwar Adam <nikhileshwar41@gmail.com>
2025-05-17 12:05:16 +0530

    Version One Changes Done

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
+// App.tsx
 import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import { store } from './src/redux_store/store';
-import AppNavigator from './src/navigation/AppNavigator';
-import useOrientationLock from './src/hooks/useOrientationLock';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/redux/store';
+import Navigation from './src/navigation/Navigation'; // Import Navigation component
 
-const App = () => {
-  useOrientationLock();
+const App: React.FC = () => {
+  console.log('App component rendered'); // Log the rendering of the App component
 
   return (
     <SafeAreaProvider>
       <Provider store={store}>
         <SafeAreaView style={{ flex: 1 }}>
-          <AppNavigator />
+          <PersistGate loading={null} persistor={persistor}>
+            <Navigation />
+          </PersistGate>
         </SafeAreaView>
       </Provider>
     </SafeAreaProvider>
